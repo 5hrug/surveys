4c58e6431c702fc1023e47fb72b9cd56822147eb
Rastislav <rastouhrin1@gmail.com>
2021-07-03 20:30:14 +0200

    working sidebar table

--- Components/SideBar/Item.tsx
+++ Components/SideBar/Item.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 interface Props {
    text: string;
@@ -11,32 +11,40 @@ interface Props {
 function Item({ text, icon, selected, onClick }: Props) {
    return (
       <Container onClick={() => onClick && onClick(text)}>
-         {selected === text ? <Selected /> : null}
+         <IconWrapper>
+            {selected === text ? <Selected /> : null}
+            <Icon icon={icon} />
+         </IconWrapper>
+         <td>
             <Title>{text}</Title>
+         </td>
       </Container>
    );
 }
 
+const IconWrapper = styled('td')`
+   position: relative;
+`;
+const Icon = styled(FontAwesomeIcon)`
+   height: 12px;
+   margin-left: 40px;
+   margin-right: -20px;
+`;
 const Selected = styled('div')`
    width: 3px;
    position: absolute;
    height: 100%;
-   min-height: 100%;
-   max-height: 100%;
-   /* height: 50px; */
+   top: 0;
    background: linear-gradient(#e66465, #9198e5);
 `;
-const Container = styled.div`
+const Container = styled.tr`
    position: relative;
-
    color: #727278;
-   display: flex;
-   align-items: center;
-   margin: 25px 0;
+   padding: 12px 0;
+   height: 50px;
 `;
 const Title = styled.a`
    font-size: 14px;
-   margin-left: 10px;
    text-decoration: none;
 `;
 export default Item;
